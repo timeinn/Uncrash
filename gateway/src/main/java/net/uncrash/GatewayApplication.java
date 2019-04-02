17a9281de7f4a0fa0621202864ebbcdbbf14fe01
Acris Liu <acrisliu@gmail.com>
2019-04-02 16:42:06 +0800

    chore: remove RouterLocator

--- gateway/src/main/java/net/uncrash/GatewayApplication.java
+++ gateway/src/main/java/net/uncrash/GatewayApplication.java
@@ -2,9 +2,6 @@ package net.uncrash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Gateway for Uncrash
@@ -15,19 +12,6 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 public class GatewayApplication {
-
-
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        //@formatter:off
-        return builder.routes()
-            .route("user-api", r -> r.path("/user/**")
-                .uri("https://user.api.uncrash.net"))
-            .route("agent-api", r -> r.path("/agent/**")
-                .uri("https://agent.api.uncrash.net"))
-            .build();
-        //@formatter:on
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
