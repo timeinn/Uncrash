87e6ffaed3fc104b37ff54cf99fc60bd922fdd47
Sendya <18x@loacg.com>
2019-08-29 15:54:39 +0800

    :bug: 查询权限信息

--- user-service/src/main/java/net/uncrash/web/controller/PermissionController.java
+++ user-service/src/main/java/net/uncrash/web/controller/PermissionController.java
@@ -2,9 +2,11 @@ package net.uncrash.web.controller;
 
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import net.uncrash.authorization.Permission;
 import net.uncrash.authorization.annotation.Authorize;
 import net.uncrash.authorization.basic.domain.DefaultPermission;
 import net.uncrash.authorization.basic.service.PermissionService;
+import net.uncrash.core.exception.NotFoundException;
 import net.uncrash.logging.api.AccessLogger;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,8 +25,11 @@ public class PermissionController {
     private final PermissionService permissionService;
 
     @GetMapping("/{id}")
+    @Authorize(action = Permission.ACTION_QUERY)
     public DefaultPermission info(@PathVariable("id") String id) {
-        return null;
+        DefaultPermission permission = permissionService.findOne(id)
+            .orElseThrow(() -> new NotFoundException("permission " + id + " not found"));
+        return permission;
     }
 
 }
